4b9afc4176bdd4585effdaa5951a3889a27ec56d
Toan-246 <n.v.toan687@gmail.com>
2022-04-07 11:33:49 +0700

    first commit

---
+++ src/main/java/com/example/springbootcustomer/service/customer/CustomerService.java
@@ -0,0 +1,32 @@
+package com.example.springbootcustomer.service.customer;
+
+import com.example.springbootcustomer.model.Customer;
+import com.example.springbootcustomer.repository.ICustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+@Service
+public class CustomerService implements ICustomerService{
+    @Autowired
+    private ICustomerRepository customerRepository;
+    @Override
+    public Iterable findAll() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Optional findById(Long id) {
+        return customerRepository.findById(id);
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    @Override
+    public void remove(Long id) {
+        customerRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/example/springbootcustomer/service/customer/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.example.springbootcustomer.service.customer;
+
+import com.example.springbootcustomer.model.Customer;
+import com.example.springbootcustomer.service.IGeneralService;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
